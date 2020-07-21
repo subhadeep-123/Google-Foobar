df04bcc962e9b08acf1c9219d4a2423fbf4ad724
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-21 17:02:10 +0530

    Updated

---
+++ Level 2/1. Hey I Already did that!/Hey, I Already Did That.py
@@ -0,0 +1,29 @@
+def solution(n, b):
+    k = len(n)
+    zlist = []
+    while True:
+        x = ''.join(sorted(n, reverse=True))
+        y = ''.join(sorted(n))
+        z10 = int(x, b) - int(y, b)
+        if b != 10:
+            n = toBaseN(z10, b)
+        else:
+            n = str(z10)
+        if len(zlist) < k:
+            n = n.zfill(k)
+        if n in zlist:
+            return len(zlist)-zlist.index(n)
+        zlist.append(n)
+
+
+def toBaseN(num, b):
+    baseNnum = []
+    while num:
+        baseNnum.insert(0, str(num % b))
+        num = num//b
+    return ''.join(baseNnum)
+
+
+# ------------TEST CASES------------
+print(solution('1211', 10))  # => 1
+print(solution('210022', 3))  # => 3
